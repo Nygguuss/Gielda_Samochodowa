1c570e778f2de86188f614cc2b38cf628003e008
4TP1 <4TP1@DESKTOP-PESOG3C>
2024-04-11 09:12:28 +0200

    Dodaj pliki projektów.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gielda_Samochodowa
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string tymczasowehaslo = "admin";
+        string tymczasowyuzytkownik = "admin";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            string username = usernameTextBox.Text;
+            string password = passwordBox.Password;
+            // Sprawdzanie danych logowania
+            
+            if (tymczasowyuzytkownik == "admin" && tymczasowehaslo == "admin")
+            {
+                MessageBox.Show("Zalogowano pomyślnie jako: " + username);
+
+                // Tworzenie i otwieranie nowego okna
+                GlownaStrona glownastrona = new GlownaStrona();
+                glownastrona.Show();
+
+                // Zamykanie bieżącego okna logowania
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Niepoprawne dane logowania. Spróbuj ponownie.");
+            }   
+        }
+    }
+}
